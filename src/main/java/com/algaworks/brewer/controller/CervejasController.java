250455803decca3f6a124a213447523678c23107
Rafael Fernando Rutsatz <rutsatz@hotmail.com.br>
2019-06-30 19:12:43 -0300

    Adicionado layouts e Layout Dialect.

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -30,9 +30,5 @@ public class CervejasController {
 		System.out.println("sku >>>>" + cerveja.getSku());
 		return "redirect:/cervejas/novo";
 	}
-	
-	@RequestMapping("/cervejas/cadastro")
-	public String cadastro() {
-		return "/cerveja/cadastro-produto";
-	}
+
 }
