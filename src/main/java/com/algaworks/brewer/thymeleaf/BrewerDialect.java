90ea159599d5dafbc32d0ad5d39a66b8b0c5db48
Rafael Fernando Rutsatz <rutsatz@hotmail.com.br>
2019-07-11 20:58:31 -0300

    [15.17] Desafio - Criado componente para paginação.

--- src/main/java/com/algaworks/brewer/thymeleaf/BrewerDialect.java
+++ src/main/java/com/algaworks/brewer/thymeleaf/BrewerDialect.java
@@ -10,6 +10,7 @@ import org.thymeleaf.standard.StandardDialect;
 import com.algaworks.brewer.thymeleaf.processor.ClassForErrorAttributeTagProcessor;
 import com.algaworks.brewer.thymeleaf.processor.MessageElementTagProcessor;
 import com.algaworks.brewer.thymeleaf.processor.OrderElementTagProcessor;
+import com.algaworks.brewer.thymeleaf.processor.PaginationElementTagProcessor;
 
 public class BrewerDialect extends AbstractProcessorDialect {
 
@@ -30,6 +31,7 @@ public class BrewerDialect extends AbstractProcessorDialect {
 		processadores.add(new ClassForErrorAttributeTagProcessor(dialectPrefix));
 		processadores.add(new MessageElementTagProcessor(dialectPrefix));
 		processadores.add(new OrderElementTagProcessor(dialectPrefix));
+		processadores.add(new PaginationElementTagProcessor(dialectPrefix));
 		return processadores;
 	}
 
---
+++ src/main/java/com/algaworks/brewer/thymeleaf/processor/PaginationElementTagProcessor.java
@@ -0,0 +1,59 @@
+package com.algaworks.brewer.thymeleaf.processor;
+
+import org.thymeleaf.context.ITemplateContext;
+import org.thymeleaf.model.IModel;
+import org.thymeleaf.model.IModelFactory;
+import org.thymeleaf.model.IProcessableElementTag;
+import org.thymeleaf.processor.element.AbstractElementTagProcessor;
+import org.thymeleaf.processor.element.IElementTagStructureHandler;
+import org.thymeleaf.templatemode.TemplateMode;
+
+public class PaginationElementTagProcessor extends AbstractElementTagProcessor {
+
+	private static final String NOME_TAG = "pagination";
+	private static final int PRECEDENCIA = 1000;
+
+	public PaginationElementTagProcessor(String dialectPrefix) {
+		/*
+		 * Modo do template. Prefixo do dialeto. Agora sim é um elemento. true por vou
+		 * usar o elemento. null pois não é um atributo. false pois não vou usar um
+		 * atributo. E por fim, a predencia de processamento do meu elemento.
+		 */
+		super(TemplateMode.HTML, dialectPrefix, NOME_TAG, true, null, false, PRECEDENCIA);
+	}
+
+	@Override
+	protected void doProcess(ITemplateContext context, IProcessableElementTag tag,
+			IElementTagStructureHandler structureHandler) {
+		/*
+		 * O ModelFactory eu uso para criar os elementos HTML, que vou adicionar dentro
+		 * do meu model.
+		 */
+		IModelFactory modelFactory = context.getModelFactory();
+
+		/* Dentro do model que vou adicionar os meus elementos HTML. */
+		IModel model = modelFactory.createModel();
+
+		/*
+		 * Cria uma nova tag th:block e adiciona dentro do meu model. No segundo e
+		 * terceiro parametros, já consigo passar um atributo para essa nova tag e
+		 * também o valor desse meu atributo.
+		 *
+		 * Isso gera o código abaixo: <th:block
+		 * th:replace="fragments/Ordenacao :: order (${pagina}, 'nome', 'Nome')"></th:
+		 * block>
+		 */
+		model.add(modelFactory.createStandaloneElementTag("th:block", "th:replace",
+				"fragments/Pagination :: pagination"));
+
+		/*
+		 * Depois de criado meu novo elemento, digo que o elemento que está com o
+		 * brewer:message deve ser substítuido pelo novo elemento que criei. O segundo
+		 * parâmetro indica se o thymelaf ainda precisa processar o novo elemento que
+		 * criei. Como tenho os elementos th:block e th:include, que são do thymeleaf,
+		 * então tenho que passar true. Se fosse somente HTML, poderia passar false.
+		 */
+		structureHandler.replaceWith(model, true);
+	}
+
+}
