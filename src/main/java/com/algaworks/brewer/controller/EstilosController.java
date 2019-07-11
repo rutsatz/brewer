9cad6282f058ed23334e7fcb42981a1ea979dbe4
Rafael Rutsatz <rafael.rutsatz@velsis.com.br>
2019-07-11 17:49:15 -0300

    [15.16] Desafio - Adicionar a pesquisa e ordenação para o cadastro de estilos.

--- src/main/java/com/algaworks/brewer/controller/EstilosController.java
+++ src/main/java/com/algaworks/brewer/controller/EstilosController.java
@@ -1,12 +1,16 @@
 package com.algaworks.brewer.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +18,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.algaworks.brewer.controller.page.PageWrapper;
+import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.model.Estilo;
+import com.algaworks.brewer.repository.Estilos;
+import com.algaworks.brewer.repository.filter.CervejaFilter;
+import com.algaworks.brewer.repository.filter.EstiloFilter;
 import com.algaworks.brewer.service.CadastroEstiloService;
 import com.algaworks.brewer.service.exception.NomeEstiloJaCadastradoException;
 
@@ -25,6 +34,9 @@ public class EstilosController {
 	@Autowired
 	private CadastroEstiloService cadastroEstiloService;
 
+	   @Autowired
+	    private Estilos estilos;
+	
 	@RequestMapping("/novo")
 	public ModelAndView novo(Estilo estilo) {
 		ModelAndView mv = new ModelAndView("estilo/CadastroEstilo");
@@ -63,6 +75,18 @@ public class EstilosController {
 
 		System.out.println(">>>> estilo: " + estilo.getNome());
 		return ResponseEntity.ok(estilo);
+	}
+	
+	@GetMapping
+    public ModelAndView pesquisar(EstiloFilter estiloFilter, BindingResult result,
+            @PageableDefault(size = 2) Pageable pageable, HttpServletRequest httpServletRequest) {
+	    ModelAndView mv = new ModelAndView("cerveja/PesquisaEstilos");
+	    
+	    PageWrapper<Estilo> paginaWrapper = new PageWrapper<>(estilos.filtrar(estiloFilter, pageable),
+	                    httpServletRequest);
+	    
+	    mv.addObject("pagina", paginaWrapper);
+	    return mv;
 	}
 
 }
--- src/main/java/com/algaworks/brewer/repository/Estilos.java
+++ src/main/java/com/algaworks/brewer/repository/Estilos.java
@@ -6,10 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.brewer.model.Estilo;
+import com.algaworks.brewer.repository.helper.estilo.EstilosQueries;
 
 @Repository
-public interface Estilos extends JpaRepository<Estilo, Long> {
+public interface Estilos extends JpaRepository<Estilo, Long>, EstilosQueries {
 
-	public Optional<Estilo> findByNomeIgnoreCase(String nome);
+    public Optional<Estilo> findByNomeIgnoreCase(String nome);
 
 }
