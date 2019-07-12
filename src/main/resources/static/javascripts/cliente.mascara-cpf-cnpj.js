665c60e0eb1260a6febce3ffba2ead6e11475cb5
Rafael Fernando Rutsatz <rutsatz@hotmail.com.br>
2019-07-12 00:10:20 -0300

    Layout padrão para objetos js.

---
+++ src/main/resources/static/javascripts/cliente.mascara-cpf-cnpj.js
@@ -0,0 +1,20 @@
+var Brewer = Brewer || {};
+
+Brewer.MascaraCpfCnpj = (function() {
+
+	function MascaraCpfCnpj() {
+
+	}
+
+	MascaraCpfCnpj.prototype.iniciar = function() {
+
+	}
+
+	return MascaraCpfCnpj;
+
+}());
+
+$(function() {
+	var mascaraCpfCnpj = new Brewer.MascaraCpfCnpj();
+	mascaraCpfCnpj.iniciar();
+});
