d59b15877f1412decec6d9d18c7952ae8f35f45d
Rafael Rutsatz <rafael.rutsatz@velsis.com.br>
2019-07-15 08:36:10 -0300

    Salvando o usuário no banco de dados.

--- src/main/java/com/algaworks/brewer/controller/UsuariosController.java
+++ src/main/java/com/algaworks/brewer/controller/UsuariosController.java
@@ -2,6 +2,7 @@ package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,25 +11,37 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Usuario;
+import com.algaworks.brewer.service.CadastroUsuarioService;
+import com.algaworks.brewer.service.exception.EmailUsuarioJaCadastradoException;
 
 @Controller
 @RequestMapping("/usuarios")
 public class UsuariosController {
 
-	@RequestMapping("/novo")
-	public ModelAndView novo(Usuario usuario) {
-		ModelAndView mv = new ModelAndView("usuario/CadastroUsuario");
-		return mv;
-	}
+    @Autowired
+    private CadastroUsuarioService cadastroUsuarioService;
 
-	@PostMapping("/novo")
-	public ModelAndView salvar(@Valid Usuario usuario, BindingResult result, RedirectAttributes attributes) {
-		if (result.hasErrors()) {
-			return novo(usuario);
-		}
+    @RequestMapping("/novo")
+    public ModelAndView novo(Usuario usuario) {
+        ModelAndView mv = new ModelAndView("usuario/CadastroUsuario");
+        return mv;
+    }
 
-		attributes.addFlashAttribute("mensagem", "Usuário salvo com sucesso");
-		return new ModelAndView("redirect:/usuarios/novo");
-	}
+    @PostMapping("/novo")
+    public ModelAndView salvar(@Valid Usuario usuario, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            return novo(usuario);
+        }
+
+        try {
+            cadastroUsuarioService.salvar(usuario);
+        } catch (EmailUsuarioJaCadastradoException e) {
+            result.rejectValue("email", e.getMessage(), e.getMessage());
+            return novo(usuario);
+        }
+
+        attributes.addFlashAttribute("mensagem", "Usuário salvo com sucesso");
+        return new ModelAndView("redirect:/usuarios/novo");
+    }
 
 }
