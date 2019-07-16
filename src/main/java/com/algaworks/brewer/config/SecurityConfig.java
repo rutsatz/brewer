7fd8a8544be0ba3ee0e2b12d7bc953178049edda
Rafael Rutsatz <rafael.rutsatz@velsis.com.br>
2019-07-16 08:53:17 -0300

    [19.6] Adicionado restrição de acesso as URLs através de ROLES.

--- src/main/java/com/algaworks/brewer/config/SecurityConfig.java
+++ src/main/java/com/algaworks/brewer/config/SecurityConfig.java
@@ -59,6 +59,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http
 				/* Eu quero autorizar requisições. */
 				.authorizeRequests()
+				/* Adiciono uma configuração de permissão. Estou dizendo que para acessar o cadastro de cidades (/cidades/nova)
+				  eu preciso ter a permissão de CADASTRAR_CIDADE. Essa é a coluna nome cadastrada na tabela permissão.
+				 Preciso cuidar a ordem. Se eu colocasse essa linha abaixo, depois do .anyRequest().authenticated(), ele
+				 iria liberar acesso sempre, mesmo sem ter a role, pois o SpringSecurity vai avaliando na sequência que eu defino. Então
+				 ele viria e olharia que ele precisa verificar as autorizações e depois olharia que para qualquer requisição,
+				 ele precisa estar autenticado. Aí, opa! Está autenticado, pode liberar. Ele nem avaliaria essa nossa regra que
+				 está depois, pois ele encontrou um match antes e liberou o acesso.
+
+				 Por isso preciso definir primeiro o que quero bloquear, e por fim deixar liberado o resto.
+				 */
+				.antMatchers("/cidades/nova").hasRole("CADASTRAR_CIDADE")
+				/* Do usuário pra frente, precisa ter a role de cadastro de usuário. Essa é uma expressão ANT. Aí tanto o cadastro
+				  quanto a pesquisa precisam dessa role. */
+				.antMatchers("/usuarios/**").hasRole("CADASTRAR_USUARIO")
 				/*
 				 * Permite qualquer um acessar os arquivos do "/layout" para frente, pois na
 				 * tela de login ele precisa carregar esses arquivos. Porém se ele tentar
@@ -90,6 +104,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				 * usuário e senha).
 				 */
 				.authenticated()
+				/* Se eu quiser bloquear qualquer outra página que tenha sobra, ao invés de liberar, eu tiro o .authenticated()
+				  e coloco o .denyAll(). Eu uso um ou outro, se deixar os dois .anyRequest(), pode ser que de problema no SpringSecutiry. */
+//				.denyAll()
 				/* Volto para o objeto anterior e posso continuar configurando. */
 				.and()
 				/* Habilito o form de login. */
