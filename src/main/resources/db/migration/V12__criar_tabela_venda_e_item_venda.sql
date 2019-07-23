358b06046e18b80c9d4a722826d2b5028786b64e
Rafael Fernando Rutsatz <rutsatz@hotmail.com.br>
2019-07-23 20:47:57 -0300

    Tratamento para exibir o valor total em vermelho quando for negativo e adicionamento o mapeamento e as tabelas venda e item_venda.

---
+++ src/main/resources/db/migration/V12__criar_tabela_venda_e_item_venda.sql
@@ -0,0 +1,24 @@
+CREATE TABLE venda (
+    codigo serial primary key,
+    data_criacao TIMESTAMP NOT NULL,
+    valor_frete DECIMAL(10,2),
+    valor_desconto DECIMAL(10,2),
+    valor_total DECIMAL(10,2) NOT NULL,
+    status VARCHAR(30) NOT NULL,
+    observacao VARCHAR(200),
+    data_entrega TIMESTAMP,
+    codigo_cliente BIGINT NOT NULL,
+    codigo_usuario BIGINT NOT NULL,
+    FOREIGN KEY (codigo_cliente) REFERENCES cliente(codigo),
+    FOREIGN KEY (codigo_usuario) REFERENCES usuario(codigo)
+);
+
+CREATE TABLE item_venda (
+    codigo serial primary key,
+    quantidade INTEGER NOT NULL,
+    valor_unitario DECIMAL(10,2) NOT NULL,
+    codigo_cerveja BIGINT NOT NULL,
+    codigo_venda BIGINT NOT NULL,
+    FOREIGN KEY (codigo_cerveja) REFERENCES cerveja(codigo),
+    FOREIGN KEY (codigo_venda) REFERENCES venda(codigo)
+);
--- src/main/resources/static/javascripts/venda.js
+++ src/main/resources/static/javascripts/venda.js
@@ -1,59 +1,73 @@
 Brewer.Venda = (function() {
-	
+
 	function Venda(tabelaItens) {
 		this.tabelaItens = tabelaItens;
 		/* Box que contém o valor total da venda. */
 		this.valorTotalBox = $('.js-valor-total-box');
 		this.valorFreteInput = $('#valorFrete');
 		this.valorDescontoInput = $('#valorDesconto');
-		
+
+		this.valorTotalBoxContainer = $('.js-valor-total-box-container');
+
 		/* Variáveis da tela. */
 		this.valorTotalItens = 0;
 		this.valorFrete = 0;
 		this.valorDesconto = 0;
 	}
-	
+
 	Venda.prototype.iniciar = function() {
-		/* Adiciona o listener para quando mudar um item na tabela de vendas, podermos recuperar o
-		 * valor total. */
-		this.tabelaItens.on('tabela-itens-atualizada', onTabelaItensAtualizada.bind(this));
-		/* Coloca um evento ao terminar de digitar no input do frete, para ir atualizando o valor do frete. */
+		/*
+		 * Adiciona o listener para quando mudar um item na tabela de vendas,
+		 * podermos recuperar o valor total.
+		 */
+		this.tabelaItens.on('tabela-itens-atualizada', onTabelaItensAtualizada
+				.bind(this));
+		/*
+		 * Coloca um evento ao terminar de digitar no input do frete, para ir
+		 * atualizando o valor do frete.
+		 */
 		this.valorFreteInput.on('keyup', onValorFreteAlterado.bind(this));
 		this.valorDescontoInput.on('keyup', onValorDescontoAlterado.bind(this));
-		
-		/* Adiciona um listener nesses mesmos eventos, para também chamar o método para atualizar os valores na tela,
-		 * quando esse evento ocorrer. */
-		this.tabelaItens.on('tabela-itens-atualizada', onValoresAlterados.bind(this));
+
+		/*
+		 * Adiciona um listener nesses mesmos eventos, para também chamar o
+		 * método para atualizar os valores na tela, quando esse evento ocorrer.
+		 */
+		this.tabelaItens.on('tabela-itens-atualizada', onValoresAlterados
+				.bind(this));
 		this.valorFreteInput.on('keyup', onValoresAlterados.bind(this));
 		this.valorDescontoInput.on('keyup', onValoresAlterados.bind(this));
 	}
-	
+
 	/* valorTotalItens é recebido lá do servidor. */
 	function onTabelaItensAtualizada(evento, valorTotalItens) {
-		/* Quando removo todos os itens, ele vem undefined. Posso testar com o null, que ele
-		 * também verifica o undefined. */
+		/*
+		 * Quando removo todos os itens, ele vem undefined. Posso testar com o
+		 * null, que ele também verifica o undefined.
+		 */
 		this.valorTotalItens = valorTotalItens == null ? 0 : valorTotalItens;
 
 	}
 
 	function onValorFreteAlterado(evento) {
-		this.valorFrete = Brewer.recuperarValor( $(evento.target).val() );
+		this.valorFrete = Brewer.recuperarValor($(evento.target).val());
 	}
 
 	function onValorDescontoAlterado(evento) {
-		this.valorDesconto = Brewer.recuperarValor( $(evento.target).val() );
+		this.valorDesconto = Brewer.recuperarValor($(evento.target).val());
 	}
-	
-	function onValoresAlterados() {
-		var valorTotal = this.valorTotalItens + this.valorFrete - this.valorDesconto;
-		/* Atualiza o valor total na tela. */
-		this.valorTotalBox.html(Brewer.formatarMoeda(valorTotal));		
-	}
-	
-	return Venda;
-	
-}());
 
+	function onValoresAlterados() {
+		var valorTotal = this.valorTotalItens + this.valorFrete	- this.valorDesconto;
+		/* Atualiza o valor total na tela. */
+		this.valorTotalBox.html(Brewer.formatarMoeda(valorTotal));
+		
+		this.valorTotalBoxContainer.toggleClass('negativo', valorTotal < 0);
+	}
+
+	return Venda;
+
+}());
 
 $(function() {
 	/**
@@ -63,12 +77,14 @@ $(function() {
 	var autocomplete = new Brewer.Autocomplete();
 	autocomplete.iniciar();
 
-	/** É inicialiado aqui, pois a venda depende da tabela de itens. A venda vai
-	 * ficar escutando alterações na tabela de itens. */
+	/**
+	 * É inicialiado aqui, pois a venda depende da tabela de itens. A venda vai
+	 * ficar escutando alterações na tabela de itens.
+	 */
 	var tabelaItens = new Brewer.TabelaItens(autocomplete);
 	tabelaItens.iniciar();
 
 	var venda = new Brewer.Venda(tabelaItens);
 	venda.iniciar();
-	
+
 });
