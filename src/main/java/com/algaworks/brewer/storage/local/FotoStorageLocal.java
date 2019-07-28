b113eec952c326ab03964a5e87497cbaedb90679
Rafael Fernando Rutsatz <rutsatz@hotmail.com.br>
2019-07-28 12:53:27 -0300

    [32.15] Usar a propriedade user.home na classe FotoStorageLocal.

--- src/main/java/com/algaworks/brewer/storage/local/FotoStorageLocal.java
+++ src/main/java/com/algaworks/brewer/storage/local/FotoStorageLocal.java
@@ -6,6 +6,8 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -30,21 +32,13 @@ public class FotoStorageLocal implements FotoStorage {
 
 	private static final String THUMBNAIL_PREFIX = "thumbnail.";
 
+	@Value("${brewer.foto-storage-local.local}")
 	private Path local;
 //	private Path localTemporario;
 
 	/* Recupera o valor lá do arquivo de properties. */
 	@Value("${brewer.foto-storage-local.url-base}")
 	private String urlBase;
-	
-	public FotoStorageLocal() {
-		this(FileSystems.getDefault().getPath(System.getenv("HOME"), ".brewerfotos"));
-	}
-
-	public FotoStorageLocal(Path path) {
-		this.local = path;
-		criarPastas();
-	}
 
 	@Override
 	public String salvar(MultipartFile[] files) {
@@ -127,6 +121,8 @@ public class FotoStorageLocal implements FotoStorage {
 		return urlBase + foto;
 	}
 
+	/* Executa o método depois que o Spring instanciar a classe. */
+	@PostConstruct
 	private void criarPastas() {
 		try {
 			Files.createDirectories(this.local);
